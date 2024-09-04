978d364edffc6afa1d98055f166dc2a9849c712f
Tamanna Bajaj <tamanna8216@gmail.com>
2024-09-04 12:38:24 +0530

    updated display

--- Calc.js
+++ Calc.js
@@ -67,5 +67,14 @@ class Calculator {
           return integerDisplay
         }
       }
-    
+      updateDisplay() {
+        this.currentOperandTextElement.innerText =
+          this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+          this.previousOperandTextElement.innerText =
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+          this.previousOperandTextElement.innerText = ''
+        }
+      }  
 }
