6175f8b4c1d27aad5c8b714d148ef153ec3a3672
Tamanna Bajaj <tamanna8216@gmail.com>
2024-09-04 12:35:52 +0530

    constructor created

--- Calc.js
+++ Calc.js
@@ -4,4 +4,17 @@ class Calculator {
       this.currentOperandTextElement = currentOperandTextElement
       this.clear()
     }
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+      }
+      delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+      }
+    
+      appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+      }
 }
